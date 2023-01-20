4a4e735e461caa451c76a1ccead1524bee87ed89
navitha <navithareddy112@gmail.com>
2023-01-20 21:23:53 +0530

    [navitha] UC1RegexPattern

---
+++ Day22_UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day22_UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration\n");
+
+            UC1RegexPattern.FirstName();
+            Console.WriteLine("\nUser Registration Program Ends");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day22_UserRegistration/UC1RegexPattern.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day22_UserRegistration
+{
+    public class UC1RegexPattern
+    {
+        public static void FirstName()
+        {
+            string[] arr = { "navi", "chitti", "njg", "navitha", "986654" };
+            string pattern = "^[A-Z][a-z]{3}";
+
+            foreach (string input in arr)
+            {
+                //IsMatch(String, String, RegexOptions)
+
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Console.WriteLine("{0} is valid", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Invalid", input);
+                }
+            }
+        }
+    }
+}
